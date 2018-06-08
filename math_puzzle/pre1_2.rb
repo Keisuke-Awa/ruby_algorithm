ef96cb1b0553adc4e014366d724f2629b6b86037
Keisuke Awazu <keisuke.awa.basket@gmail.com>
2018-06-08 09:30:53 +0900

    pre1_2.rb

---
+++ math_puzzle/pre1_2.rb
@@ -0,0 +1,28 @@
+require 'benchmark'
+
+## 1つのテーブルに配置できる最大の人数が10人の時、100人が1つ以上のテーブルに分かれて座る人数のパターンを求めてください。
+
+MAX_TABLE_PEOPLE, PEOPLE_NUM = 10, 100
+
+@memo = {}
+def check(remain:, pre:)
+
+  return @memo[[remain, pre]] if @memo[[remain, pre]]
+
+  return 0 if remain < 0
+  return 1 if remain == 0
+
+  count = 0
+  pre.upto(MAX_TABLE_PEOPLE) do |i|
+    count += check(remain: remain - i, pre: i)
+  end
+  @memo[[remain, pre]] = count
+end
+
+Benchmark.bm 10 do |r|
+  r.report "pre1_2" do
+    puts check(remain: PEOPLE_NUM, pre: 2)
+  end
+end
+
+
